0eb939b1cce7d1e391ca56357eca35553d5c72c0
RealKingTino <tmowizzy@gmail.com>
2023-06-22 12:43:04 -0700

    fixed betty errors

--- rotr.c
+++ rotr.c
@@ -1,22 +1,29 @@
 #include "monty.h"
 
+/**
+ * rotr - Rotates the stack to the bottom
+ * @stack: Double pointer to the top of the stack
+ * @line_number: Line number of the rotr opcode
+ *
+ * RealKingTino
+ */
 void rotr(stack_t **stack,  __attribute__((unused))  unsigned int line_number)
 {
-        stack_t *temp = *stack;
+	stack_t *temp = *stack;
 	stack_t *second_last = NULL;
 
-        if (*stack == NULL || (*stack)->next == NULL)
-          return;
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
 
-        while (temp->next != NULL)
-        {
+	while (temp->next != NULL)
+	{
 		second_last = temp;
-          temp = temp->next;
-        }
+		temp = temp->next;
+	}
 
-        temp->next = *stack;
-        (*stack)->prev = temp;
+	temp->next = *stack;
+	(*stack)->prev = temp;
 	second_last->next = NULL;
 	temp->prev = NULL;
-        *stack = temp;
+	*stack = temp;
 }
